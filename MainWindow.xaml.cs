57d47d1143690edfdbeec8fb9219dc1fbb541a7a
redleaf1221 <redleaf1221@outlook.com>
2024-12-18 22:44:54 +0800

    Adding some minor features: About, Mode switch, Version switch

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,9 +16,49 @@ namespace DamageV2
     /// </summary>
     public partial class MainWindow : Window
     {
+        String CurrentVersion = "1.8";
+        String CurrentMode = "Average";
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e) {
+            About.Header = "By MathTangent";
+            Action<MenuItem> action = async (MenuItem about) => {
+                await Task.Delay(500);
+                about.Header = "About";
+            };
+            About.Dispatcher.BeginInvoke(action, About);
+        }
+
+        private void ChangeVersion_Click(object sender, RoutedEventArgs e) {
+            if (CurrentVersion == "1.8") {
+                CurrentVersion = "1.9";
+                ChangeVersion.Content = "Current Version: 1.9";
+            } else if (CurrentVersion == "1.9") {
+                CurrentVersion = "1.8";
+                ChangeVersion.Content = "Current Version: 1.8";
+            } else {
+                throw new Exception("How do we get there? Wrong version.");
+            }
+            return;
+        }
+
+        private void ChangeMode_Click(object sender, RoutedEventArgs e) {
+            if(CurrentMode == "Average") {
+                CurrentMode = "DPS";
+                ChangeMode.Content = "Current Mode: DPS";
+            } else if(CurrentMode == "DPS") {
+                CurrentMode = "Max";
+                ChangeMode.Content = "Current Mode: Max Damage";
+            } else if(CurrentMode == "Max") {
+                CurrentMode = "Average";
+                ChangeMode.Content = "Current Mode: Average Damage";
+            } else {
+                throw new Exception("How do we get there? Wrong mode.");
+            }
+            return;
         }
     }
 }
